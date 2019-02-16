b3a63d2ca8d06da3ca0b703bd11c60bab82586ef
xyx0826 <xyx0826@hotmail.com>
2019-02-16 15:28:05 -0500

    添加项目文件。

---
+++ Mileslice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mileslice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
